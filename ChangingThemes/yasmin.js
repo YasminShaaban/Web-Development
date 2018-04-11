270b2c9381cfcfdc28e9e1dddd9ea20019393e77
Yasmin Shaaban <yasminshaaban3@gmail.com>
2018-04-12 01:48:06 +0200

    new web examples commit 2

---
+++ ChangingThemes/yasmin.js
@@ -0,0 +1,22 @@
+function boy(){
+ table=document.getElementById("table");
+ tableHead=document.getElementById("tableHead");
+ item1=document.getElementById("item1");
+ item2=document.getElementById("item2");
+ table.className="boyTable";
+ tableHead.className="boyHead";
+ item2.className="boyItems";
+ item1.className="boyItems";
+}
+
+function girl(){
+table=document.getElementById("table");
+ tableHead=document.getElementById("tableHead");
+ item1=document.getElementById("item1");
+ item2=document.getElementById("item2");
+ table.className="girlTable";
+ tableHead.className="girlHead";
+ item2.className="girlItems";
+ item1.className="girlItems";
+
+}
--- Movies/movies.js
+++ Movies/movies.js
@@ -6,3 +6,22 @@ function createList(){
   divMovies=document.getElementById("divMovies");
   divMovies.innerHTML="<ul>  <li>Youssef</li>   <li>Mahmoud</li> </ul>";
 }
+
+//everything in html is node : text node or element node
+function createListByNodes(){
+    //use for loop better
+   title=document.createTextNode("Here is the list :");
+   list=document.createElement("ul");
+   text1=document.createTextNode("Mahmoud");
+   item1=document.createElement("li");
+   item2=document.createElement("li");
+   text2=document.createTextNode("Youssef");
+   item1.appendChild(text1);
+   item2.appendChild(text2);
+   list.appendChild(item2);
+   list.appendChild(item1);
+   divMovies=document.getElementById("divMovies");
+   divMovies.appendChild(title);
+   divMovies.appendChild(list);
+
+}
